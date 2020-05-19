31aa681f0ec43b33ffc6e6371d5279ef622962fc
Collins Muriuki <murerwacollins@gmail.com>
2020-05-19 11:03:37 +0300

    Create user login and sign up mutations

--- src/resolvers/mutations/user.mutation.js
+++ src/resolvers/mutations/user.mutation.js
@@ -1,6 +1,50 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import * as dotenv from "dotenv";
+
+dotenv.config({
+  path: ".env",
+});
+
 export default {
   signUp: async (parent, { data }, { sequelize: { User } }) => {
-    const user = await User.create({ ...data });
-    return user;
+    const { firstName, lastName, email, password } = data;
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = User.create({
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+      });
+      return user;
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+
+  login: async (parent, { data }, { sequelize: { User } }) => {
+    const { email, password } = data;
+    try {
+      const user = await User.findOne({
+        where: {
+          email: email,
+        },
+      });
+
+      if (!user) throw new Error("Wrong email or password");
+
+      const validPassword = await bcrypt.compare(password, user.password);
+
+      if (validPassword) {
+        const token = jwt.sign({ user }, process.env.JWT_SECRET);
+
+        return { token, user };
+      } else {
+        throw new Error("Wrong password");
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
   },
 };
