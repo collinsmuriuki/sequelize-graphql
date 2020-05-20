acf205eb3f46cff54902e6ab3c50d676c69abec1
Collins Muriuki <murerwacollins@gmail.com>
2020-05-20 15:34:46 +0300

    Add changePassword mutaion

--- src/resolvers/mutations/user.mutation.js
+++ src/resolvers/mutations/user.mutation.js
@@ -19,7 +19,7 @@ export default {
       });
       return user;
     } catch (error) {
-      throw new Error(error);
+      throw new Error(error.toString());
     }
   },
 
@@ -52,7 +52,7 @@ export default {
         throw new Error("Wrong password");
       }
     } catch (error) {
-      throw new Error(error);
+      throw new Error(error.toString());
     }
   },
 
@@ -74,7 +74,34 @@ export default {
       });
       return user;
     } catch (error) {
-      throw new Error(error);
+      throw new Error(error.toString());
+    }
+  },
+
+  changePassword: async (parent, { data }, { sequelize, req }) => {
+    const { oldPassword, newPassword, userId } = data;
+    const { User } = sequelize;
+    if (!req.user || req.user.id != userId) throw new Error("Unauthorized");
+    try {
+      // get user instance from id
+      const userInstance = await User.findByPk(userId);
+      // compare hash
+      const valid = await bcrypt.compare(oldPassword, userInstance.password);
+      // throw if old password is incorrect
+      if (!valid) throw new Error("Old password does not match!");
+      // store new hash
+      userInstance.password = await bcrypt.hash(newPassword, 10);
+      console.log(userInstance.password)
+      await userInstance.save();
+      const user = await User.findOne({
+        where: {
+          id: userId,
+        },
+        include: [{ all: true, nested: true }],
+      });
+      return user;
+    } catch (error) {
+      throw new Error(error.toString());
     }
   },
 };
