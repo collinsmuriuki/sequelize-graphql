1f139eafe147855c3c58afcc15f5b62e96f3983d
Collins Muriuki <murerwacollins@gmail.com>
2020-05-20 16:17:26 +0300

    Error handle mutations

--- src/resolvers/mutations/comment.mutation.js
+++ src/resolvers/mutations/comment.mutation.js
@@ -1,16 +1,21 @@
 export default {
   createComment: async (parent, { data }, { sequelize, req }) => {
     const { Comment } = sequelize;
-    if (!req.user ||req.user.id !== data.UserId) throw new Error("Unauthorized");
-    const comment = await Comment.create({
-      ...data,
-    });
-    const result = await Comment.findOne({
-      where: {
-        id: comment.id,
-      },
-      include: [{ all: true }],
-    });
-    return result;
+    if (!req.user || req.user.id !== data.UserId)
+      throw new Error("Unauthorized");
+    try {
+      const comment = await Comment.create({
+        ...data,
+      });
+      const result = await Comment.findOne({
+        where: {
+          id: comment.id,
+        },
+        include: [{ all: true }],
+      });
+      return result;
+    } catch (error) {
+      throw new Error(error.toString());
+    }
   },
 };
--- src/resolvers/mutations/post.mutation.js
+++ src/resolvers/mutations/post.mutation.js
@@ -2,15 +2,19 @@ export default {
   createPost: async (parent, { data }, { sequelize: { Post }, req }) => {
     if (!req.user || req.user.id !== data.UserId)
       throw new Error("Unauthorized");
-    const post = await Post.create({
-      ...data,
-    });
-    const result = await Post.findOne({
-      where: {
-        id: post.id,
-      },
-      include: [{ all: true }],
-    });
-    return result;
+    try {
+      const post = await Post.create({
+        ...data,
+      });
+      const result = await Post.findOne({
+        where: {
+          id: post.id,
+        },
+        include: [{ all: true }],
+      });
+      return result;
+    } catch (error) {
+      throw new Error(error.toString());
+    }
   },
 };
