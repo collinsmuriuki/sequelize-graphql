96c095a7c35948cd7da4e564284be75a7d2d986c
Collins Muriuki <murerwacollins@gmail.com>
2020-05-26 18:28:15 +0300

    Spring cleaning

--- src/schema.ts
+++ src/schema.ts
@@ -1,10 +1,9 @@
 import { makeExecutableSchema } from "graphql-tools";
 
 import resolvers from "./resolvers/index";
-// @ts-ignore
 import * as schema from "./schema.graphql";
 
 export default makeExecutableSchema({
-    typeDefs: [schema],
-    resolvers
-})
+  typeDefs: [schema],
+  resolvers,
+});
