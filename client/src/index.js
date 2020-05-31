032e0400d78110956a244ae68976767fab460dea
Collins Muriuki <murerwacollins@gmail.com>
2020-05-31 10:46:07 +0300

    Install and setup semantic-ui

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { ApolloProvider } from "react-apollo";
 
-import "./index.css";
+import "semantic-ui-css/semantic.min.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import client from "./graphql/config";
