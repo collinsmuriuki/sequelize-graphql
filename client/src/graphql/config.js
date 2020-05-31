5aae190c360cf036ed22179139e95528ef819052
Collins Muriuki <murerwacollins@gmail.com>
2020-05-31 10:34:08 +0300

    Configure  Apollo client & react-router-dom

---
+++ client/src/graphql/config.js
@@ -0,0 +1,22 @@
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { persistCache } from "apollo-cache-persist";
+import { ApolloClient } from "apollo-boost";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:4002",
+});
+
+const cache = new InMemoryCache();
+
+persistCache({
+  cache,
+  storage: window.localStorage,
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+export default client;
