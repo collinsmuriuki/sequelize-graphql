f8465abf01458b4bf2a1cb723befc4d132e8a9c0
Collins Muriuki <murerwacollins@gmail.com>
2020-05-31 17:43:52 +0300

    Configure http headers with apollo-link-cotext

--- client/src/graphql/config.js
+++ client/src/graphql/config.js
@@ -2,6 +2,7 @@ import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { persistCache } from "apollo-cache-persist";
 import { ApolloClient } from "apollo-boost";
+import { setContext } from "apollo-link-context";
 
 import { resolvers } from "./resolvers";
 
@@ -16,8 +17,20 @@ persistCache({
   storage: window.localStorage,
 });
 
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache,
   resolvers,
 });
