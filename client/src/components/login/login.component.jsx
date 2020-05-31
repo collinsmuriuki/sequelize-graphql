fac7e8247d3badcbd7e73f295abceaad5488e917
Collins Muriuki <murerwacollins@gmail.com>
2020-05-31 17:38:43 +0300

    Create login and sign up form components

--- client/src/components/login/login.component.jsx
+++ client/src/components/login/login.component.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Button, Form } from "semantic-ui-react";
 
 const Login = () => {
-  return <div>hello from login</div>;
+  return (
+    <Form>
+      <Form.Field>
+        <label>Email</label>
+        <input placeholder="First Name" />
+      </Form.Field>
+      <Form.Field>
+        <label>Password</label>
+        <input type="password" placeholder="Password" />
+      </Form.Field>
+      <Button type="submit">Login</Button>
+    </Form>
+  );
 };
 
 export default Login;
--- client/src/components/singup/signup.component.jsx
+++ client/src/components/singup/signup.component.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Form, Button } from "semantic-ui-react";
 
 const SignUp = () => {
-  return <div>hello from sign up</div>;
+  return (
+    <Form>
+      <Form.Field>
+        <label>First Name</label>
+        <input placeholder="First Name" />
+      </Form.Field>
+      <Form.Field>
+        <label>Last Name</label>
+        <input placeholder="Last Name" />
+      </Form.Field>
+      <Form.Field>
+        <label>Email</label>
+        <input type="email" placeholder="Email" />
+      </Form.Field>
+      <Form.Field>
+        <label>Password</label>
+        <input type="password" placeholder="Password" />
+      </Form.Field>
+      <Button type="submit">Sign Up</Button>
+    </Form>
+  );
 };
 
 export default SignUp;
